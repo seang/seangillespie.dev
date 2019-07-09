c8ba9d4b183869f05b92b295701990916e0979cf
apokryphon <s.gillespie@apokryphon.com>
2019-07-09 19:04:14 -0400

    Update Bablio Image

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
 import seangillespie from '../images/seangillespie.jpg'
-import Bablio from '../images/bablio.jpg'
+import Bablio from '../images/bablio.png'
 import resume from '../images/seangillespie-resume-2019.pdf'
 
 class Main extends React.Component {
@@ -34,10 +34,9 @@ class Main extends React.Component {
         >
           <h2 className="major">Work</h2>
           <p>
-            Sean is currently expanding his skillset as a Native Mobile
-            Developer by day and refining his Full-Stack Javascript Kung-Fu by
-            night while always curious to explore the emerging AR/VR/XR
-            technology landscape.
+            Sean is a Native Mobile Developer by day and refining his Full-Stack
+            Javascript Kung-Fu by night while always curious to explore the
+            emerging AR/VR/XR technology landscape.
           </p>
           <span className="image main">
             <img src={Bablio} alt="" />
